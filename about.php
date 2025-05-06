c1dec586209185a2b85d9f4252a4e5d8320943b4
Kofi Owusu Ankomah <Yodakblues75@gmail.com>
2025-05-06 08:31:15 -0700

    Added About and Contact pages

--- about.php
+++ about.php
@@ -1,4 +1,4 @@
 <?php include 'includes/header.php'; ?>
 <h1>About Us</h1>
-<p>This is a simple LAMP stack web application built with PHP, Apache, and MySQL.</p>
+<p>This is the About page.</p>
 <?php include 'includes/footer.php'; ?>
