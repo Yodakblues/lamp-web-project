7facc06d54073a9cb9f8c9981e7c8db0b7e0e112
Kofi Owusu Ankomah <Yodakblues75@gmail.com>
2025-05-06 08:23:59 -0700

    Initial commit with basic LAMP project structure

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-<?php echo 'Hello, LAMP Project'; ?>
+<?php include 'includes/header.php'; ?>
+<h1>Welcome to My LAMP Web Project</h1>
+<p>This is the homepage.</p>
+<?php include 'includes/footer.php'; ?>
