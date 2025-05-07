c66edc481d61b9e7e89e73583a053fe8e7fa8e30
Kofi Owusu Ankomah <Yodakblues75@gmail.com>
2025-05-07 06:06:42 -0700

    Add public_html files

---
+++ public_html/about.php
@@ -0,0 +1,4 @@
+<?php include 'includes/header.php'; ?>
+<h1>About Us</h1>
+<p>This is the About page.</p>
+<?php include 'includes/footer.php'; ?>
---
+++ public_html/includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LAMP Web Project</title>
+</head>
+<body>
+    <header>
+        <h2>My LAMP Web Project</h2>
+        <nav>
+            <a href="index.php">Home</a> |
+            <a href="about.php">About</a> |
+            <a href="contact.php">Contact</a>
+        </nav>
+        <hr>
+    </header>
